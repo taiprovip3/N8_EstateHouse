eb793fe7cde124aeea04d2e83c361c2ee851de3b
NVPL1234 <nlam39784@gmail.com>
2022-04-29 23:20:39 +0700

    finish RegisterScreen.java, LoginScreen.java, ProfieScreen.java 100%

--- app/src/main/java/com/example/estatehouse/LoginScreen.java
+++ app/src/main/java/com/example/estatehouse/LoginScreen.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,29 +18,39 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
-    private EditText edit_name;
-    private EditText edit_pass;
+    private EditText txtName, txtPass;
     private FirebaseAuth auth;
-    private Button btn_log;
+    private Button btnLog;
+    private TextView txtRegisterHere;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
         auth=FirebaseAuth.getInstance();
-        edit_name=findViewById(R.id.edit_name);
-        edit_pass=findViewById(R.id.edit_pass);
-        btn_log=findViewById(R.id.btn_log);
-        btn_log.setOnClickListener(new View.OnClickListener() {
+        txtName=(EditText) findViewById(R.id.txtName);
+        txtPass=(EditText) findViewById(R.id.txtPass);
+        txtRegisterHere=(TextView)findViewById(R.id.txtRegisterHere);
+        txtRegisterHere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email=edit_name.getText().toString();
-                final String pass=edit_pass.getText().toString();
+                Intent intent=new Intent(LoginScreen.this, RegisterScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnLog=(Button) findViewById(R.id.btnLog);
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=txtName.getText().toString();
+                String pass=txtPass.getText().toString();
                 if (TextUtils.isEmpty(email)){
-                    Toast.makeText(getApplicationContext(),"Nhập user name",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Enter user name",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (TextUtils.isEmpty(pass)){
-                    Toast.makeText(getApplicationContext(),"Nhập mật khẩu",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Enter password",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 auth.signInWithEmailAndPassword(email,pass)
@@ -47,12 +58,7 @@ public class LoginScreen extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (!task.isSuccessful()){
-                                    if (pass.length()<6){
-                                        edit_pass.setError(getString(R.string.minium_password));
-                                    }else {
-                                        Toast.makeText(LoginScreen.this,getString(R.string.auth_fail),
-                                                Toast.LENGTH_SHORT).show();
-                                    }
+                                    Toast.makeText(getApplicationContext(),"Login failed",Toast.LENGTH_SHORT).show();
                                 }
                                 else {
                                     Intent intent=new Intent(LoginScreen.this,HomepageScreen.class);
