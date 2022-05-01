ee268e6cbb6515d70df7edadb7e4efd49f393791
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-05-01 15:09:39 +0700

    finish Note Screen.java

--- app/src/main/java/com/example/estatehouse/HomepageScreen.java
+++ app/src/main/java/com/example/estatehouse/HomepageScreen.java
@@ -1,15 +1,10 @@
 package com.example.estatehouse;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -19,7 +14,6 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.estatehouse.adapter.HomepageAdapter;
@@ -28,15 +22,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -47,9 +37,6 @@ import java.util.Map;
 public class HomepageScreen extends AppCompatActivity {
 
     FirebaseFirestore db;
-    FirebaseAuth mAuth;
-    FirebaseUser currentUser;
-    CollectionReference houseRef;
     Spinner spinnerCountries, spinnerCities;
     RadioGroup rdGroupType;
     RadioButton rdBuy, rdRent;
@@ -59,16 +46,6 @@ public class HomepageScreen extends AppCompatActivity {
     ImageView helpView, languageView, sellerView, loginView, homePageView, notiView, profileView;
     Button btnFilter;
     private int typeSelectedDefault = 0;
-    AlertDialog dialogLanguage;
-    AlertDialog.Builder builderLanguage;
-    String[] languages = {"US (default)", "VN"};
-    String languageSelected = "";
-    TextView txtViewHelp, txtViewLanguage, txtViewSeller, txtViewlogin, txtViewTitle, txtViewLocation, txtViewType, txtViewRecommend, txtViewHomepage, txtViewNoti, txtViewProfile;
-
-    TextView [] componentTextView;
-    List<TextView> arrayTextViewComponent;
-    String [] componentVNTranslate;
-    String [] componentUSTranslate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,7 +53,22 @@ public class HomepageScreen extends AppCompatActivity {
         setContentView(R.layout.activity_homepage_screen);
 
         //Ánh xạ
-        anhXa();
+        db = FirebaseFirestore.getInstance();
+        CollectionReference houseRef = db.collection("House");
+        helpView = findViewById(R.id.hp_helpIcon);
+        languageView = findViewById(R.id.hp_languageIcon);
+        sellerView = findViewById(R.id.hp_sellerIcon);
+        loginView = findViewById(R.id.hp_loginIcon);
+        homePageView = findViewById(R.id.hp_homePageIcon);
+        notiView = findViewById(R.id.hp_notiIcon);
+        profileView = findViewById(R.id.hp_profileIcon);
+        spinnerCountries = findViewById(R.id.spinner_countries);
+        spinnerCities = findViewById(R.id.spinner_cities);
+        rdGroupType = findViewById(R.id.rdGroupType);
+        btnFilter = findViewById(R.id.hp_btnFilter);
+        rdBuy = findViewById(R.id.radioButtonBuy);
+        rdRent = findViewById(R.id.radioButtonRent);
+        listView = findViewById(R.id.cart_listView);
 
         ArrayAdapter<CharSequence> adapterCountry = ArrayAdapter.createFromResource(this, R.array.countries, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapterCity = ArrayAdapter.createFromResource(this, R.array.cities, android.R.layout.simple_spinner_item);
@@ -89,76 +81,26 @@ public class HomepageScreen extends AppCompatActivity {
         helpView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomepageScreen.this, HelpScreen.class);
-                startActivity(intent);
+                Toast.makeText(HomepageScreen.this, "Change intent to helpScreen", Toast.LENGTH_LONG).show();
             }
         });
         languageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                builderLanguage = new AlertDialog.Builder(HomepageScreen.this);
-                builderLanguage.setTitle("Avaiable languages ...");
-                builderLanguage.setSingleChoiceItems(languages, -1, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        languageSelected = languages[i];
-                    }
-                });
-                builderLanguage.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                    }
-                });
-                builderLanguage.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        if(languageSelected.equalsIgnoreCase("VN")){
-                            for (int k = 0; k < componentVNTranslate.length; k++)
-                                arrayTextViewComponent.get(k).setText(componentVNTranslate[k]);
-                            btnFilter.setText("Tìm kiếm");
-                            rdBuy.setText("MUA");
-                            rdRent.setText("THUÊ");
-                            Toast.makeText(HomepageScreen.this, "Ngôn ngữ của bạn đã được thay đổi thành :: " + languageSelected, Toast.LENGTH_LONG).show();
-                        } else{
-                            for (int k = 0; k < componentVNTranslate.length; k++)
-                                arrayTextViewComponent.get(k).setText(componentUSTranslate[k]);
-                            btnFilter.setText("Filter");
-                            rdBuy.setText("BUY");
-                            rdRent.setText("RENT");
-                            Toast.makeText(HomepageScreen.this, "Your language have been changed to :: " + languageSelected, Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-                dialogLanguage = builderLanguage.create();
-                dialogLanguage.show();
+                Toast.makeText(HomepageScreen.this, "Language avaiable: US, VN", Toast.LENGTH_LONG).show();
             }
         });
         sellerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomepageScreen.this, SellerScreen.class);
-                startActivity(intent);
+                Toast.makeText(HomepageScreen.this, "Change to 'SellerScreen.java'", Toast.LENGTH_SHORT).show();
             }
         });
         loginView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!isLogged()){
-                    Intent intent = new Intent(HomepageScreen.this, LoginScreen.class);
-                    startActivity(intent);
-                }
-            }
-        });
-        txtViewlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(isLogged()){
-                    txtViewlogin.setText("Login");
-                    mAuth.signOut();
-                    Intent intent = new Intent(HomepageScreen.this, LoginScreen.class);
-                    startActivity(intent);
-                    Toast.makeText(HomepageScreen.this, "Logout success", Toast.LENGTH_LONG).show();
-                }
+                Intent intent = new Intent(HomepageScreen.this, LoginScreen.class);
+                startActivity(intent);
             }
         });
         homePageView.setOnClickListener(new View.OnClickListener() {
@@ -176,13 +118,7 @@ public class HomepageScreen extends AppCompatActivity {
         profileView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(isLogged()){
-                    Intent intent = new Intent(HomepageScreen.this, ProfieScreen.class);
-                    startActivity(intent);
-                } else{
-                    Intent intent = new Intent(HomepageScreen.this, LoginScreen.class);
-                    startActivity(intent);
-                }
+                Toast.makeText(HomepageScreen.this, "Change to ProfileScreen", Toast.LENGTH_LONG).show();
             }
         });
         rdGroupType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -253,68 +189,7 @@ public class HomepageScreen extends AppCompatActivity {
                             Log.w("ERROR-FIRE", "Error getting documents.", task.getException());
                     }
                 });
-
     }//end onCreate
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if(isLogged()){
-            txtViewlogin.setText("Logout");
-            txtViewlogin.getPaint().setUnderlineText(true);
-            txtViewlogin.setTextColor(Color.BLUE);
-
-        } else{
-            txtViewlogin.setText("Login");
-            txtViewlogin.getPaint().setUnderlineText(false);
-            txtViewlogin.setTextColor(Color.BLACK);
-        }
-    }//end onCreate
-
-    private boolean isLogged() {
-        currentUser = mAuth.getCurrentUser();
-        if(currentUser == null)
-            return false;
-        return true;
-    }
-
-    private void anhXa() {
-        db = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
-        houseRef = db.collection("House");
-        helpView = findViewById(R.id.hp_helpIcon);
-        languageView = findViewById(R.id.hp_languageIcon);
-        sellerView = findViewById(R.id.hp_sellerIcon);
-        loginView = findViewById(R.id.hp_loginIcon);
-        homePageView = findViewById(R.id.hp_homePageIcon);
-        notiView = findViewById(R.id.hp_notiIcon);
-        profileView = findViewById(R.id.hp_profileIcon);
-        spinnerCountries = findViewById(R.id.spinner_countries);
-        spinnerCities = findViewById(R.id.spinner_cities);
-        rdGroupType = findViewById(R.id.rdGroupType);
-        btnFilter = findViewById(R.id.hp_btnFilter);
-        rdBuy = findViewById(R.id.radioButtonBuy);
-        rdRent = findViewById(R.id.radioButtonRent);
-        listView = findViewById(R.id.cart_listView);
-        txtViewHelp = findViewById(R.id.hp_txtViewHelp);
-        txtViewLanguage = findViewById(R.id.hp_txtViewLanguage);
-        txtViewSeller = findViewById(R.id.hp_txtViewSeller);
-        txtViewlogin = findViewById(R.id.hp_txtViewLogin);
-        txtViewTitle = findViewById(R.id.hp_txtViewTitle);
-        txtViewLocation = findViewById(R.id.hp_txtViewLocation);
-        txtViewType = findViewById(R.id.hp_txtViewType);
-        txtViewRecommend = findViewById(R.id.hp_txtViewRecommend);
-        txtViewHomepage = findViewById(R.id.hp_txtViewHomepage);
-        txtViewNoti = findViewById(R.id.hp_txtViewNoti);
-        txtViewProfile = findViewById(R.id.hp_txtViewProfile);
-        componentTextView = new TextView[] {txtViewHelp, txtViewLanguage, txtViewSeller, txtViewlogin, txtViewTitle, txtViewLocation, txtViewType, txtViewRecommend, txtViewHomepage, txtViewNoti, txtViewProfile};
-        arrayTextViewComponent = new ArrayList<TextView>();
-        componentVNTranslate = new String[] {"Trợ giúp", "Ngôn ngữ", "Bán hàng", "Đăng nhập", "Lựa Chọn Căn Nhà Tuyệt Nhất", "Vị trí", "Loại thuê", "Được đề xuất cho bạn", "Trang chủ", "Thông báo(s)", "Cá nhân"};
-        componentUSTranslate = new String[] {"Help", "Language", "Seller", "Login", "Find Your Best House", "Location", "Type", "Recommended for your", "Homepage", "Notification(s)", "Profile"};
-        for (int k = 0; k < componentTextView.length; k++) {
-            arrayTextViewComponent.add(componentTextView[k]);
-        }
-    }
 
 
     private void addNewDocument(String fn, String ln) {
---
+++ app/src/main/java/com/example/estatehouse/NoticScreen.java
@@ -0,0 +1,14 @@
+package com.example.estatehouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notic_screen);
+    }
+}
