dabd301b52afc695140d37c54f66676b99d44156
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-05-01 17:04:32 +0700

    finish Note Screen.java

--- app/src/main/java/com/example/estatehouse/NoticScreen.java
+++ app/src/main/java/com/example/estatehouse/NoticScreen.java
@@ -2,13 +2,24 @@ package com.example.estatehouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NoticScreen extends AppCompatActivity {
-
+    ImageView btn_img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notic_screen);
+        btn_img=findViewById(R.id.btn_img);
+        btn_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(NoticScreen.this,HomepageScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
